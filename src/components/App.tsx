2b7208f7c9d3a2b7b335fc5241932fff3f9ba733
RicardoAlmeida25 <ricardo@Ricardos-MacBook-Pro.local>
2019-07-17 17:57:50 +0100

    Add font-awesome support.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,17 @@
 import React, { FunctionComponent } from 'react';
 import { AppWrapper, Header, Title } from './App.styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 const App: FunctionComponent = () => {
   return (
     <AppWrapper>
       <Header>
         <Title>Developer</Title>
+        <FontAwesomeIcon icon={faGithub} color="palevioletred" />
+        <FontAwesomeIcon icon={faLinkedin} color="palevioletred" />
+        <FontAwesomeIcon icon={faEnvelope} color="palevioletred" />
       </Header>
     </AppWrapper>
   );
